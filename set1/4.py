d5e253150185c1dad32848f243825d73458c459a
BREEEEET <26woodbrec@washk12.org>
2025-01-09 07:23:24 -0800

    Add files via upload

---
+++ set1/4.py
@@ -0,0 +1,7 @@
+num = int(input("number :  "))
+if num <= 10 and num >=1:
+    print('Between 1 and 10')
+elif num <=20 and num >= 11:
+    print("between 11 and 20")
+else:
+    print("somewhere else.")
